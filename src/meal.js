fc3fe294376169be1ddb5fed5754a6aaa75bea47
Rachel Hendricks <>
2021-02-04 19:12:45 -0700

    Add meal.js

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,49 @@
-function nameMenuItem() {
+function nameMenuItem(name) {
+  return `Delicious ${name}`;
+}
+var menuName = nameMenuItem();
 
+function createMenuItem(menuName, menuPrice, menuType) {
+  return menuItem = {name:[menuName],
+  price: `${menuPrice}`,
+  type: `${menuType}`,};
+}
+var ingredients = [];
+
+function addIngredients(newIngredients, ingredients) {
+  for(var i = 0; i < ingredients.length; i++) {
+    if(ingredients[i] === newIngredients) {
+  return ingredients;
+}
+}
+  ingredients.push(newIngredients);
+  return ingredients.length, ingredients;
+}
+var newMenuItem = createMenuItem();
+var initialPrice = [newMenuItem.price];
+
+function formatPrice(initialPrice) {
+  return `$${initialPrice}`;
 }
 
+function decreasePrice(initialPrice) {
+  var changePrice = initialPrice * .10;
+  return initialPrice - changePrice;
+}
+var menuItemType = [newMenuItem.type];
+function createRecipe(menuName, ingredients, menuItemType) {
+  return recipe = {
+    title: menuName,
+    ingredients: ingredients,
+    type: menuItemType,
+  };
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
-
-
