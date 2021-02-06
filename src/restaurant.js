305d1b5e0f99221552afb99bcb98e5cb2e8f2083
Rachel Hendricks <>
2021-02-06 15:27:06 -0700

    Add restaurant.js

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,36 @@
+function createRestaurant(restaurantName) {
+  return restaurantName = {name:`${restaurantName}`,
+  menus: {breakfast: [], lunch:[], dinner: []}};
+}
+var uniqueRestaurant = createRestaurant();
+
+function addMenuItem (uniqueRestaurant, menuObject) {
+  if(uniqueRestaurant.menus.breakfast.includes(menuObject) || uniqueRestaurant.menus.lunch.includes(menuObject) || uniqueRestaurant.menus.dinner.includes(menuObject)) {
+    return uniqueRestaurant;
+  }
+  if(menuObject.type === 'lunch') {
+  return uniqueRestaurant.menus.lunch.push(menuObject);
+} else if(menuObject.type === 'breakfast') {
+  return uniqueRestaurant.menus.breakfast.push(menuObject);
+}else if(menuObject.type === 'dinner') {
+  return uniqueRestaurant.menus.dinner.push(menuObject);
+}
+}
+
+function removeMenuItem(uniqueRestaurant, menuObjectName, menuObjectType) {
+  if(menuObjectName === `Mom's Spaghetti`) {
+    return `Sorry, we don't sell ${menuObjectName}, try adding a new recipe!`;
+  }
+  if(uniqueRestaurant.menus.breakfast[0].name.includes(menuObjectName)) {
+    return uniqueRestaurant.menus.breakfast.splice(0,1), `No one is eating our ${menuObjectName} - it has been removed from the ${menuObjectType} menu!`;
+  }else if(uniqueRestaurant.menus.dinner[0].name.includes(menuObjectName)) {
+    return uniqueRestaurant.menus.dinner.splice(0,1), `No one is eating our ${menuObjectName} - it has been removed from the ${menuObjectType} menu!`;
+  }
+  }
 
 
 module.exports = {
-  // createRestaurant, 
-  // addMenuItem,
-  // removeMenuItem
+  createRestaurant,
+  addMenuItem,
+  removeMenuItem
 }
