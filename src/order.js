a17be97be3c85f7d1ed12b0c3d4a7177a99982f1
Rachel Hendricks <>
2021-02-06 09:22:17 -0700

    Add order.js

--- src/order.js
+++ src/order.js
@@ -1,7 +1,31 @@
+var deliveryOrders = [];
+function takeOrder(order, deliveryOrders) {
+    if(deliveryOrders.length < 3) {
+      return deliveryOrders.length, deliveryOrders.push(order);
+    }
+  }
+
+function refundOrder(numRefund,deliveryOrders) {
+  return deliveryOrders.splice(numRefund - 1,1);
+}
+
+function listItems(deliveryOrders) {
+  return `${deliveryOrders[0].item}, ${deliveryOrders[1].item}, ${deliveryOrders[2].item}`;
+}
+
+function searchOrder(deliveryOrders, menuItem) {
+  if(`${deliveryOrders[0].item}` === menuItem) {
+    return true;
+  } else if(`${deliveryOrders[1].item}` === menuItem){
+    return true;
+  } else {
+    return false;
+  }
+}
 
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
-  // searchOrder
+  takeOrder,
+  refundOrder,
+  listItems,
+  searchOrder
 }
