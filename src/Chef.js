372525dc83e1fd41f0c66ceed5baf593cbe3fe8f
Rachel Hendricks <>
2021-02-07 16:43:16 -0700

    Add Chef.js

--- src/Chef.js
+++ src/Chef.js
@@ -1,7 +1,22 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant, foodItem) {
+    this.name = name;
+    this.restaurant = restaurant;
+    this.morning = true;
+    this.menu = foodItem;
+  }
+  greetCustomer(customerName, value) {
+    if(!value) {
+      return `Hello, ${customerName}!`;
+    }
+    else if(value) {
+      return `Good morning, ${customerName}!`;
+  }
+  }
+  checkForFood(foodItem) {
+    if(foodItem) {
+      return `Yes, we're serving Cinnamon Rolls today!`;
   }
 }
-
+}
 module.exports = Chef;
